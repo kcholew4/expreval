2873ec9ad4921f1dd95c4d921d3712ae5cb79abc
Kamil Cholewa <kcholew4@gmail.com>
2023-10-22 17:11:54 +0200

    Create AST pretty printer

---
+++ tools/astPrinter.ts
@@ -0,0 +1,33 @@
+import { Binary, Grouping, NumberLiteral, Unary, Visitor, Expr } from "../expr";
+import { Token, TokenType } from "../token";
+
+class AstPrinter implements Visitor<string> {
+  visitBinaryExpr(expr: Binary): string {
+    return `${expr.left.accept(this)} ${expr.operator.toString()} ${expr.right.accept(this)}`;
+  }
+  visitGroupingExpr(expr: Grouping): string {
+    return `(${expr.expression.accept(this)})`;
+  }
+  visitNumberLiteraExpr(expr: NumberLiteral): string {
+    return expr.value.toString();
+  }
+  visitUnaryExpr(expr: Unary): string {
+    return `${expr.operator.toString()} ${expr.right.accept(this)}}`;
+  }
+
+  print(expression: Expr) {
+    return expression.accept(this);
+  }
+}
+
+const expression = new Binary(
+  new Grouping(
+    new Binary(new NumberLiteral(2), new Token(TokenType.PLUS, "+"), new NumberLiteral(3))
+  ),
+  new Token(TokenType.MUL, "*"),
+  new NumberLiteral(2)
+);
+
+const astPrinter = new AstPrinter();
+
+console.log(astPrinter.print(expression));
