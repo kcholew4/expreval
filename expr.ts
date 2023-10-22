426ad66f274e76f4ce99cfb82a9ca288b8b647b4
Kamil Cholewa <kcholew4@gmail.com>
2023-10-22 16:32:39 +0200

    Create AST classes

---
+++ expr.ts
@@ -0,0 +1,70 @@
+import { Token } from "./token";
+
+export interface Visitor<T> {
+  visitBinaryExpr(expr: Binary): T;
+  visitGroupingExpr(expr: Grouping): T;
+  visitNumberLiteraExpr(expr: NumberLiteral): T;
+  visitUnaryExpr(expr: Unary): T;
+}
+
+export abstract class Expr {
+  abstract accept<T>(visitor: Visitor<T>): T;
+}
+
+export class Binary extends Expr {
+  readonly left: Expr;
+  readonly operator: Token;
+  readonly right: Expr;
+
+  constructor(left: Expr, operator: Token, right: Expr) {
+    super();
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+  }
+
+  accept<T>(visitor: Visitor<T>) {
+    return visitor.visitBinaryExpr(this);
+  }
+}
+
+export class Grouping extends Expr {
+  readonly expression: Expr;
+
+  constructor(expression: Expr) {
+    super();
+    this.expression = expression;
+  }
+
+  accept<T>(visitor: Visitor<T>) {
+    return visitor.visitGroupingExpr(this);
+  }
+}
+
+export class NumberLiteral extends Expr {
+  readonly value: number;
+
+  constructor(value: number) {
+    super();
+    this.value = value;
+  }
+
+  accept<T>(visitor: Visitor<T>) {
+    return visitor.visitNumberLiteraExpr(this);
+  }
+}
+
+export class Unary extends Expr {
+  readonly operator: Token;
+  readonly right: Expr;
+
+  constructor(operator: Token, right: Expr) {
+    super();
+    this.operator = operator;
+    this.right = right;
+  }
+
+  accept<T>(visitor: Visitor<T>) {
+    return visitor.visitUnaryExpr(this);
+  }
+}
